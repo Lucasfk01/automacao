daa9cafed24a043239f2dff1676d2fe61ce80dd5
Lucasfk01 <61986067+Lucasfk01@users.noreply.github.com>
2020-07-13 11:55:08 -0300

    Delete dados_login.rb

--- dados_login.rb
+++
@@ -1,8 +0,0 @@
-Before "@dados_login" do
-    @email = "lucasfkimura@hotmail.com"
-    @senha = "Luquinhas@01"
-    @nome = "Lucas"
-  
-    @email_invalido = "teste@teste.com.br"
-    @senha_invalida = "123456"
-end
